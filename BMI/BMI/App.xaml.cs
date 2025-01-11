ae485a264c22374bc35d09b9d8013858ec41a623
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2025-01-11 20:20:28 +0100

    Initial Commit

---
+++ BMI/BMI/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BMI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
