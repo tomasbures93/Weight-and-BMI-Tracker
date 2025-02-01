e75498db01d3a8e6d26fcdf197b0898a35ac59cc
BurcisWolf <98662129+BurcisWolf@users.noreply.github.com>
2025-02-01 03:59:47 +0100

    Naming and UI fixes

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -9,19 +9,11 @@ namespace BMI
         {
             InitializeComponent();
             welcomeMessage.Text = "Welcome " + AppData.User.Name;
-
-#if ANDROID
-            // For Android platform, load the Android-specific view
-            Content = new MainPage().Content;
-#elif WINDOWS
-            // For Windows platform, load the Windows-specific view
-            Content = new MainPageWindows().Content;
-#else
-            // Default view for other platforms
-            Content = new Label { Text = "Default view" };
-#endif
         }
-
+        private async void NavigateToSetup(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Setup());
+        }
 
 
 
