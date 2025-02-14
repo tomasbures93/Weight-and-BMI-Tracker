13adcd337fb1d4e54223a928040ac71ef5e19a0b
Tomas Bures <98662129+tomasbures93@users.noreply.github.com>
2025-02-14 15:08:09 +0100

    Update MainPage.xaml.cs

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -5,6 +5,17 @@ namespace BMI
 {
     public partial class MainPage : ContentPage
     {
+        /*
+        TO DO
+        1) AboutPage
+        2) AppShell rework
+        3) BMI page delete
+        4) ChartPage
+        5) MainPage
+        6) SetupPage
+        7) SaveData ( File / AppProperties )
+        8) Proper Test
+        */
         public MainPage()
         {
             InitializeComponent();
